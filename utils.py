79d35bda5939e925364ce55bcca01e45964d5a5e
Ksu <kseniyaartemova@yandex.ru>
2023-07-27 15:58:52 +0200

    created directory logs with readme.txt, added added maximum log file size limit

--- utils.py
+++ utils.py
@@ -8,11 +8,15 @@ def basic_logger(msg, level=logging.INFO, encoding="UTF-8"):
         logger = logging.getLogger(__name__)
         logger.setLevel(level)
         handler = RotatingFileHandler('logs/query_log.log',
-                                      maxBytes=1_000_000,
+                                      maxBytes=100_000,
+                                      backupCount=1,
                                       encoding=encoding)
         handler.setFormatter(logging.Formatter(format))
         logger.addHandler(handler)
         logger.info(f"Query:{msg}")
+
+    except PermissionError as error:
+        pass
 
     except Exception as error:
         print(f"\033[31mОшибка логирования: {error}\033[0m")
